d06a439c2505d9c0914e3da0f4f0b6bc269bba37
Aayush Yadav <aayush130405@gmail.com>
2025-06-14 12:40:57 +0530

    Add dotenv support and update .gitignore

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
+import 'dotenv/config';
 import express from 'express';
-const port=3000;
+const port=process.env.PORT||3000;
 const app=express();
 app.use(express.json()); //using body data of frontend
 
